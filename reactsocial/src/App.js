a9de10ce93b0adb6a76a12b2386c419d19640429
mymhsrep3355 <muhammadhamzasalahuddin@gmail.com>
2023-05-19 23:38:45 +0500

    Hi, its my fisrt commit

---
+++ reactsocial/src/App.js
@@ -0,0 +1,19 @@
+// import Login from "./pages/login/Login";
+// function App() {
+//   return (
+//     <div>
+//       <Login></Login>
+//     </div>
+//   );
+// }
+
+import Login from "./pages/login";
+
+// export default App;
+
+const App = () =>{
+  return (
+    <Login/>
+  );
+}
+export default App;
---
+++ reactsocial/src/pages/login/Login.jsx
@@ -0,0 +1,27 @@
+import "./login.scss"
+
+const Login = () => {
+  return (
+    <div className="login">
+        Login
+    </div>
+  );
+}
+
+export default Login
+{/* <div className="container">
+            <div className="left">
+                <h1>Hello World.</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil fugiat ipsam voluptatum alias culpa aliquid ipsum cum harum dolorem incidunt distinctio dolores, consequuntur architecto quod expedita illo accusantium quia beatae.</p>
+            </div>
+            <span>Do you have an account?</span>
+            <button>Register</button>
+            <div className="right">
+                <h1>Login</h1>
+                <form action="">
+                    <input type="text" placeholder="Username" name="" id="" />
+                    <input type="text" placeholder="Password"name="" id="" />
+                    <button>Login</button>
+                </form>
+            </div>
+        </div> */}
